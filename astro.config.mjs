c19772417830079f63d737d9689ac6961e1ffe11
Anfitriao Airbnb <anfitrioesairbnb@gmail.com>
2026-03-30 10:53:49 -0300

    Fix deploy build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
+// Alterado para saída estática (padrão) para melhor performance e compatibilidade com Vercel
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
 });

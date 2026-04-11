07a91ccbad71c93e5500d3dce5b32855c4398baf
Anfitriao Airbnb <anfitrioesairbnb@gmail.com>
2026-04-11 08:50:00 -0300

    Perf: forçando o CSS a carregar inline para evitar bloqueio de renderização no PageSpeed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import tailwind from '@astrojs/tailwind';
 // Alterado para saída estática (padrão) para melhor performance e compatibilidade com Vercel
 export default defineConfig({
   integrations: [tailwind()],
+  build: {
+    inlineStylesheets: 'always'
+  }
 });
